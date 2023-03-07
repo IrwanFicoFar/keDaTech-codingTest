9605ef6954f9a24d7870fda28d9a52f2ea439364
IrwanFicoFar <irwanhadi9@gmail.com>
2023-03-07 19:00:14 +0700

    fix: problem1 , 2, 3, and 4

--- Part1/problem1/problem1.ts
+++ Part1/problem1/problem1.ts
@@ -1,3 +1,18 @@
+/*
+  - looping huruf dengan batasan panjang huruf tersebut
+  - jika huruf index ke i-1 ada spasi, maka huruf pertama + index ke i (jika ada spasi sebelumnya)
+  - masukkan ke varibale penampung lalu return
+*/
+
 export const acronym = (phrase: string) => {
-  // your code here
+  // simpen huruf yang sebelumnya ada spasi
+  let afterSpace = "";
+  for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i - 1] == " ") {
+      afterSpace += phrase[i];
+    }
+  }
+  // phrase index pertama ditambah dengan huruf yang sudah di simpan di afterSpace lalu di upperCase
+  let finalDisplay = phrase[0] + afterSpace;
+  return finalDisplay.toUpperCase();
 };
