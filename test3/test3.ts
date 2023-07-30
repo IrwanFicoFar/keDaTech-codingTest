f449ecb67d7968291c80d4dc3344dfcfd86dbc86
IrwanFicoFar <irwanhadi9@gmail.com>
2023-07-30 21:27:08 +0800

    comment console.log

--- test3/test3.ts
+++ test3/test3.ts
@@ -17,12 +17,12 @@ export const bilangGenapNested = (obj: any) => {
   return sum;
 };
 
-console.log(
-  bilangGenapNested({
-    a: 2,
-    b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
-    c: { c: { c: 2 }, cc: "ball", ccc: 5 },
-    d: 1,
-    e: { e: { e: 4 }, ee: "car" },
-  })
-);
+// console.log(
+//   bilangGenapNested({
+//     a: 2,
+//     b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+//     c: { c: { c: 2 }, cc: "ball", ccc: 5 },
+//     d: 1,
+//     e: { e: { e: 4 }, ee: "car" },
+//   })
+// );
