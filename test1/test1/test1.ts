9455a9ae5606c9a5bab871d545d5c32711ca5bd6
IrwanFicoFar <irwanhadi9@gmail.com>
2023-07-30 20:40:06 +0800

    test 1

---
+++ test1/test1/test1.ts
@@ -0,0 +1,18 @@
+export const urutAngkaDariTerbesar = (num: number[]): number[] => {
+  const numLength = num.length;
+
+  for (let i = 0; i < numLength - 1; i++) {
+    for (let j = i + 1; j < numLength; j++) {
+      console.log(num[i], num[j]);
+      if (num[i] < num[j]) {
+        let temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+      }
+    }
+  }
+
+  return num;
+};
+
+// console.log(urutAngkaDariTerbesar([1, 2, 4, 3, 5, 3, 2, 1]));
